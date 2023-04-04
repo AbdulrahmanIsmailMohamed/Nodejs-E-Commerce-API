e5ed432f9b2fb975371f27dfce3bb03b757628d8
Abdulrahman Ismail <abdulrahman.ismail.mohammed@gmail.com>
2023-04-04 05:44:33 +0200

    Update Image Processing

--- middlewares/imageProcessingMW.js
+++ middlewares/imageProcessingMW.js
@@ -1,6 +1,7 @@
 const sharp = require("sharp");
 const { v4: uuidv4 } = require('uuid')
 const asyncHandler = require("./asyncHandler");
+const APIError = require("../util/APIError");
 
 const imageProcessing = (
     imageNameFolder,
@@ -11,25 +12,23 @@ const imageProcessing = (
     width = 600
 ) =>
     asyncHandler(async (req, res, next) => {
-        const uniqueSuffix = `${imageNameFolder}--${uuidv4()}--${Date.now()}.${format}`;
-
         if (req.file) {
+            const uniqueSuffix = `${imageNameFolder}--${uuidv4()}--${Date.now()}.${format}`;
             await sharp(req.file.buffer)
                 .resize(width, hight)
                 .toFormat(`${format}`, { quality: quality })
                 // .jpeg({ quality: 90 })
                 .toFile(`uploads/${nameFolder}/${uniqueSuffix}`);
+
+            // save image into our db
+            //* url: http://localhost:3333/api/v1/categories/category--036615f5-c390-48fb-99ce-2d03330bb47f--1680570794277.jpeg
+            const api = process.env.API
+            const basePath = `${req.protocol}://${req.get('host')}${api}/${nameFolder}/${uniqueSuffix}`;
+            req.body.image = basePath;
+            next();
+        } else {
+            next(new APIError("Not Found Image", 400));
         }
-
-        // save image into our db
-        //* url: http://localhost:3333/api/v1/categories/category--036615f5-c390-48fb-99ce-2d03330bb47f--1680570794277.jpeg
-        
-        const api = process.env.API
-        const basePath = `${req.protocol}://${req.get('host')}${api}/${nameFolder}/${uniqueSuffix}`;
-        req.body.image = basePath;
-        
-        next();
     });
-
 
 module.exports = imageProcessing;
