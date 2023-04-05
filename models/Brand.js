d6750cac8c4f6739a97e7cda0bfd3ce2ae6a2c70
Abdulrahman Ismail <abdulrahman.ismail.mohammed@gmail.com>
2023-04-05 05:24:46 +0200

    Mongoose MW POSR Event:Save

--- models/Brand.js
+++ models/Brand.js
@@ -20,6 +20,16 @@ const brandSchema = mongoose.Schema(
     { timestamps: true }
 );
 
+
+// * *middleware to set url in image and it start when to create brand
+
+brandSchema.post("save", (doc) => {
+    if (doc.image) {
+        const baseurl = `${process.env.BASE_URL}${process.env.API}/brands/${doc.image}`;
+        doc.image = baseurl
+    }
+})
+
 const Brand = mongoose.model("Brand", brandSchema);
 
 module.exports = Brand;
