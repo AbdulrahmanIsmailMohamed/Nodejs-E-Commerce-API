27532bcd518391f54b61987a716918e257daf613
AbdulrahmanIsmailMohamed <abdulrahman.ismail.mohammed@gmail.com>
2023-04-26 14:00:22 +0200

    Order Schema

---
+++ src/models/Order.js
@@ -0,0 +1,60 @@
+const { Schema, model } = require("mongoose");
+
+const orderSchema = Schema(
+    {
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: [true, "The User Id Must Be Not Null"]
+        },
+        cartItem: [
+            {
+                product: {
+                    type: Schema.Types.ObjectId,
+                    ref: 'Product',
+                    required: [true, "The Product Id Must Be Not Null"]
+                },
+                quantity: Number,
+                color: String,
+                price: Number,
+            },
+        ],
+        taxPrice: {
+            type: Number,
+            default: 0,
+        },
+        // shippingAddress: {
+        //     details: String,
+        //     phone: String,
+        //     city: String,
+        //     postalCode: String,
+        // },
+        shippingPrice: {
+            type: Number,
+            default: 0,
+        },
+        totalOrderPrice: {
+            type: Number,
+        },
+        paymentMethodType: {
+            type: String,
+            enum: ['card', 'cash'],
+            default: 'cash',
+        },
+        isPaid: {
+            type: Boolean,
+            default: false,
+        },
+        paidAt: Date,
+        isDelivered: {
+            type: Boolean,
+            default: false,
+        },
+        deliveredAt: Date,
+    },
+    { timestamps: true }
+);
+
+const Order = model("Order", orderSchema);
+
+module.exports = Order;
